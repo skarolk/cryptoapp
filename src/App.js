e9518c1e38c798fde37e0d84629c94f0cc080613
skarolk <skarolkiewicz@gmail.com>
2022-06-07 10:19:17 -0500

    rendering mocked data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// Amplify client library has various API categories tat can be imported
+// Auth for authentication
+// Storage for S3 storage
+// API for interacting with REST and GraphQL
+import { API } from "aws-amplify";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [coins, updateCoins] = useState([]);
+
+  async function fetchCoins() {
+    const data = await API.get("cryptoapi", "/coins");
+    updateCoins(data.coins);
+  }
+
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>
+            {coin.name} - {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
     </div>
   );
 }
